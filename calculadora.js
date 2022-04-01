62b36388a3f4528d1585cdbb3fd4190c0ffc1539
ncorrearoese <niicolecorrearoese@gmail.com>
2022-04-01 09:07:16 -0300

    1.0.1

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,21 @@
 var somar = require("./modulos/somar");
 
 console.log(somar(20,67));
+
+
+
+var subtrair = require("./modulos/subtrair");
+
+console.log(subtrair(60,40));
+
+
+
+var multiplicar = require("./modulos/multiplicar");
+
+console.log(multiplicar(60,40));
+
+
+
+var dividir = require("./modulos/dividir");
+
+console.log(dividir(8,4));
